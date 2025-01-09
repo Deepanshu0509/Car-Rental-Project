0cdd263b13474812ff47d130cd4e7be5929b0601
Deepanshu <deepanshu.kewalramani2@gmail.com>
2025-01-09 22:54:49 +0530

    post mapping in backend

--- rent-a-car/src/main/java/com/coderdot/controllers/AdminController.java
+++ rent-a-car/src/main/java/com/coderdot/controllers/AdminController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ public class AdminController {
     @Autowired
     private AdminService adminService;
 
+    @PostMapping("/car")
     public ResponseEntity<?> postCar(@ModelAttribute CarDto carDto) {
         boolean success = adminService.postCar(carDto);
         if(success){
