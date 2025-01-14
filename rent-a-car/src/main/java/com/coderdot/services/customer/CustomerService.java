5abeccc5cf6acac8fc740c48ed9d39dc10cf2c3f
Deepanshu <deepanshu.kewalramani2@gmail.com>
2025-01-14 11:40:27 +0530

    got all the cars on dashboard

---
+++ rent-a-car/src/main/java/com/coderdot/services/customer/CustomerService.java
@@ -0,0 +1,9 @@
+package com.coderdot.services.customer;
+
+import com.coderdot.dtos.CarDto;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<CarDto> getAllCars();
+}
---
+++ rent-a-car/src/main/java/com/coderdot/services/customer/CustomerServiceImpl.java
@@ -0,0 +1,21 @@
+package com.coderdot.services.customer;
+
+import com.coderdot.dtos.CarDto;
+import com.coderdot.entities.Car;
+import com.coderdot.repositories.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomerServiceImpl implements CustomerService{
+    @Autowired
+    private CarRepository carRepository;
+
+    @Override
+    public List<CarDto> getAllCars() {
+        return carRepository.findAll().stream().map(Car::getCarDto).collect(Collectors.toList());
+    }
+}
