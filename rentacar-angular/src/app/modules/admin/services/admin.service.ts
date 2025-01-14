6e91575274b26fa9282cf01efbffcb366cff59fd
Deepanshu <deepanshu.kewalramani2@gmail.com>
2025-01-14 10:41:43 +0530

    update api in angular

--- rentacar-angular/src/app/modules/admin/services/admin.service.ts
+++ rentacar-angular/src/app/modules/admin/services/admin.service.ts
@@ -40,6 +40,13 @@ export class AdminService {
     });
   }
 
+  updateCar(carId: number, carDto: any){
+    return this.http.put(`${BASIC_URL}/api/admin/car/${carId}`, carDto, {
+      headers: this.createAuthorizationHeader()
+    });
+
+  }
+
   createAuthorizationHeader() {
     let authHeaders: HttpHeaders = new HttpHeaders();
     return authHeaders.set(
